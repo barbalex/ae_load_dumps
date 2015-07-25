3f5c4354a5b9b5311acabf53c61e50e11778ffb8
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-07-25 09:20:58 +0200

    delete attachments before upload

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ var adb = nano.db.use('artendb')
 
 var fileList = fs.readdirSync('./dumps')
 
+function insertAttachment (file, rev, docName, fileName) {
+  adb.attachment.insert(docName, fileName, file, 'text/plain', { rev: rev }, function (error, result) {
+    if (error) return console.log('error inserting attachment:', error)
+    console.log('result from inserting ' + fileName + ':', result)
+  })
+}
+
+function removeAttachmentThenInsert (file, rev, docName, fileName) {
+  adb.attachment.destroy(docName, fileName, { rev: rev }, function (error, body) {
+    if (error) return console.log('error removing attachment:', error)
+    console.log('body:', body)
+    insertAttachment(file, body.rev, docName, fileName)
+  })
+}
+
 fileList.forEach(function (fileName) {
   var docName = fileName.substr(0, fileName.indexOf('.'))
   var fileUrl = './dumps/' + fileName
@@ -20,10 +35,11 @@ fileList.forEach(function (fileName) {
     if (error) return console.log('error getting file:', error)
     adb.get(docName, function (error, doc) {
       if (error) return console.log('error fetching ae_objekte:', error)
-      adb.attachment.insert(docName, fileName, file, 'text/plain', { rev: doc._rev }, function (error, result) {
-        if (error) return console.log('error inserting attachment:', error)
-        console.log('result from inserting ' + fileName + ':', result)
-      })
+      if (doc._attachments && doc._attachments[fileName]) {
+        removeAttachmentThenInsert(file, doc._rev, docName, fileName)
+      } else {
+        insertAttachment(file, doc._rev, docName, fileName)
+      }
     })
   })
 })
