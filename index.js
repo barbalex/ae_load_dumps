1c09f6ea8dc69e077b2c10a9871426be01c08352
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-08-18 15:24:30 +0200

    load split dumps

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var nano = require('nano')(url)
 var adb = nano.db.use('artendb')
 
 var fileList = fs.readdirSync('./dumps')
+var counter = -5000
 
 function insertAttachment (file, rev, docName, fileName) {
   adb.attachment.insert(docName, fileName, file, 'text/plain; charset=utf8', { rev: rev }, function (error, result) {
@@ -28,18 +29,34 @@ function removeAttachmentThenInsert (file, rev, docName, fileName) {
   })
 }
 
-fileList.forEach(function (fileName) {
-  var docName = fileName.substr(0, fileName.indexOf('.'))
+function processFile (fileName) {
+  console.log('prosessing file', fileName)
   var fileUrl = './dumps/' + fileName
   fs.readFile(fileUrl, function (error, file) {
     if (error) return console.log('error getting file:', error)
+    var docName = fileName.substr(0, fileName.indexOf('_'))
     adb.get(docName, function (error, doc) {
-      if (error) return console.log('error fetching ae_objekte:', error)
-      if (doc._attachments && doc._attachments[fileName]) {
-        removeAttachmentThenInsert(file, doc._rev, docName, fileName)
+      if (error) {
+        adb.insert({
+          '_id': docName,
+          'Typ': 'GruppeDb'
+        }, function (error, result) {
+          if (error) console.log('error inserting fileName', fileName)
+          insertAttachment(file, result.rev, docName, fileName)
+        })
       } else {
-        insertAttachment(file, doc._rev, docName, fileName)
+        if (doc._attachments && doc._attachments[docName]) {
+          removeAttachmentThenInsert(file, doc._rev, docName, fileName)
+        } else {
+          insertAttachment(file, doc._rev, docName, fileName)
+        }
       }
     })
   })
-})
+}
+
+// how to get loop to tic every x milliseconds: http://brackets.clementng.me/post/24150213014/example-of-a-javascript-closure-settimeout-inside
+for (var i = 1; i <= fileList.length; i++) {
+  var fileName = fileList[i - 1]
+  setTimeout(function (fileName) { return function () { processFile(fileName) } }(fileName), 1000 * i)
+}
